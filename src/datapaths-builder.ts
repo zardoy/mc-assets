f9abb0425c285f94c0338594b1f4b41e677ca644
Vitaly <vital2580@icloud.com>
2024-07-25 13:43:57 +0300

    fix on win, add build notes

--- src/datapaths-builder.ts
+++ src/datapaths-builder.ts
@@ -1,12 +1,12 @@
-import { getVersionList, MinecraftVersion, install, installAssets, installVersion } from "@xmcl/installer";
-import { MinecraftLocation, Version, MinecraftFolder } from "@xmcl/core";
-import { versionToNumber } from './consumer/utils';
-import fs from 'fs';
-import path from 'path';
-import NodeStreamZip from 'node-stream-zip';
+import { getVersionList, MinecraftVersion, install, installAssets, installVersion } from "@xmcl/installer"
+import { MinecraftLocation, Version, MinecraftFolder } from "@xmcl/core"
+import { versionToNumber } from './consumer/utils'
+import fs from 'fs'
+import path from 'path/posix'
+import NodeStreamZip from 'node-stream-zip'
 import looksSame from 'looks-same'
 
-let prevVersion: MinecraftVersion | undefined;
+let prevVersion: MinecraftVersion | undefined
 // everything for data/
 let versionsDataPaths = {
     latest: {}
@@ -26,11 +26,11 @@ const processVersionData = async (version: MinecraftVersion, isLatest: boolean) 
     if (versionsDataPaths[version.id]) return
     console.log('check version', version.id)
     const resolvedVersion = await installVersion(version, 'temp', { side: 'client', })
-    const unarchivePath = path.join(...resolvedVersion.pathChain, 'out');
+    const unarchivePath = path.join(...resolvedVersion.pathChain, 'out')
     if (!fs.existsSync(unarchivePath)) {
-        console.log(`extracting jar ${version.id}`);
+        console.log(`extracting jar ${version.id}`)
         await extractJar(path.join(...resolvedVersion.pathChain, `${resolvedVersion.id}.jar`), unarchivePath)
-        console.log(`extracting done ${version.id}`);
+        console.log(`extracting done ${version.id}`)
     }
     // fs.renameSync(path.join(...resolvedVersion.pathChain, 'versions', resolvedVersion.id), path.join(...resolvedVersion.pathChain, 'versions', 'latest'));
 
@@ -60,21 +60,21 @@ const processVersionData = async (version: MinecraftVersion, isLatest: boolean) 
         const dataPathType = versionedPathDst
         versionsDataPaths.latest![dataPathType] ??= {}
         const processDir = async (addPath: string) => {
-            const sourcePath = path.join(...resolvedVersion.pathChain, 'out', versionedPathSrc, addPath);
-            if (!fs.existsSync(sourcePath)) return;
+            const sourcePath = path.join(...resolvedVersion.pathChain, 'out', versionedPathSrc, addPath)
+            if (!fs.existsSync(sourcePath)) return
             const files = fs.readdirSync(sourcePath, { withFileTypes: true })
             for (const fileInfo of files) {
                 if (!fileInfo.isFile()) {
-                    await processDir(path.join(addPath, fileInfo.name));
-                    continue;
+                    await processDir(path.join(addPath, fileInfo.name))
+                    continue
                 }
-                const file = fileInfo.name;
-                const sourceFilePath = path.join(sourcePath, file);
-                const targetFilePath = path.join('data', resolvedVersion.id, versionedPathDst, addPath, file);
+                const file = fileInfo.name
+                const sourceFilePath = path.join(sourcePath, file)
+                const targetFilePath = path.join('data', resolvedVersion.id, versionedPathDst, addPath, file)
                 const fileKey = path.join(patchAddPath(addPath), file)
                 if (prevVersion) {
                     const latestTargetFilePathRelative = versionsDataPaths.latest![dataPathType]![fileKey]
-                    const latestTargetFilePath = latestTargetFilePathRelative && path.join('data', latestTargetFilePathRelative);
+                    const latestTargetFilePath = latestTargetFilePathRelative && path.join('data', latestTargetFilePathRelative)
 
                     const skipExists = () => {
                         // console.debug('skipping', file);
@@ -86,12 +86,12 @@ const processVersionData = async (version: MinecraftVersion, isLatest: boolean) 
                         if (isImage) {
                             if ((await looksSame(sourceFilePath, latestTargetFilePath, { strict: true })).equal) {
                                 skipExists()
-                                continue;
+                                continue
                             }
                         } else {
                             if (fs.readFileSync(sourceFilePath, 'utf8') === fs.readFileSync(latestTargetFilePath, 'utf8')) {
                                 skipExists()
-                                continue;
+                                continue
                             }
                         }
                         versionsDataPaths[version.id] ??= {}
@@ -100,59 +100,59 @@ const processVersionData = async (version: MinecraftVersion, isLatest: boolean) 
                     }
                 }
                 versionsDataPaths.latest![dataPathType]![fileKey] = path.join(version.id, versionedPathDst, addPath, file)
-                fs.mkdirSync(path.dirname(targetFilePath), { recursive: true });
-                fs.copyFileSync(sourceFilePath, targetFilePath);
+                fs.mkdirSync(path.dirname(targetFilePath), { recursive: true })
+                fs.copyFileSync(sourceFilePath, targetFilePath)
             }
         }
-        await processDir('');
+        await processDir('')
     }
     for (const [src, dst] of Object.entries(allVersionsMergedData)) {
         // output: always latest/dst
         // if txt: merge lines, if dir: copy all files from all versions, if json: merge all jsons
-        const srcPath = path.join(...resolvedVersion.pathChain, 'out', src);
-        const dstPath = path.join('data', 'latest', dst);
-        if (!fs.existsSync(srcPath)) continue;
+        const srcPath = path.join(...resolvedVersion.pathChain, 'out', src)
+        const dstPath = path.join('data', 'latest', dst)
+        if (!fs.existsSync(srcPath)) continue
         if (fs.statSync(srcPath).isDirectory()) {
-            fs.mkdirSync(dstPath, { recursive: true });
+            fs.mkdirSync(dstPath, { recursive: true })
             const files = fs.readdirSync(srcPath, { withFileTypes: true })
             for (const fileInfo of files) {
                 if (!fileInfo.isFile()) {
-                    continue;
+                    continue
                 }
-                const file = fileInfo.name;
-                const sourceFilePath = path.join(srcPath, file);
-                const targetFilePath = path.join(dstPath, file);
-                fs.mkdirSync(path.dirname(targetFilePath), { recursive: true });
-                fs.copyFileSync(sourceFilePath, targetFilePath);
+                const file = fileInfo.name
+                const sourceFilePath = path.join(srcPath, file)
+                const targetFilePath = path.join(dstPath, file)
+                fs.mkdirSync(path.dirname(targetFilePath), { recursive: true })
+                fs.copyFileSync(sourceFilePath, targetFilePath)
             }
             continue
         }
     }
     if (isLatest) {
         for (const [src, dst] of Object.entries(copyLatest)) {
-            const srcPath = path.join(...resolvedVersion.pathChain, 'out', src);
-            const dstPath = path.join('data', 'latest', dst);
-            fs.mkdirSync(path.dirname(dstPath), { recursive: true });
-            fs.copyFileSync(srcPath, dstPath);
+            const srcPath = path.join(...resolvedVersion.pathChain, 'out', src)
+            const dstPath = path.join('data', 'latest', dst)
+            fs.mkdirSync(path.dirname(dstPath), { recursive: true })
+            fs.copyFileSync(srcPath, dstPath)
         }
     }
-    prevVersion = version;
-    console.log(`done ${version.id}`);
+    prevVersion = version
+    console.log(`done ${version.id}`)
 }
 
-const fromPath = (...paths: string[]) => (...newPaths: string[]) => path.join(...paths, ...newPaths);
+const fromPath = (...paths: string[]) => (...newPaths: string[]) => path.join(...paths, ...newPaths)
 
 const getRelativePath = (absolute: string, relativeTo: string) => {
-    const relative = path.relative(relativeTo, absolute);
-    if (relative.startsWith('..')) throw new Error(`relative path is outside of relativeTo: ${relative}`);
-    return relative;
+    const relative = path.relative(relativeTo, absolute)
+    if (relative.startsWith('..')) throw new Error(`relative path is outside of relativeTo: ${relative}`)
+    return relative
 }
 
 interface CustomExtractor {
-    name: string;
-    input: string;
-    outputFile: string;
-    extractor: (input: string) => Promise<string>;
+    name: string
+    input: string
+    outputFile: string
+    extractor: (input: string) => Promise<string>
 }
 
 const customExtracters = []
@@ -162,50 +162,50 @@ const extractJar = (filePath, outputPath) => {
         const zip = new NodeStreamZip({
             file: filePath,
             storeEntries: true
-        });
+        })
 
         zip.on('ready', () => {
             zip.extract(null, outputPath, err => {
                 if (err) {
-                    console.log('Extraction error:', err);
-                    reject(err);
+                    console.log('Extraction error:', err)
+                    reject(err)
                 } else {
                     // console.log('Extraction complete');
-                    resolve();
+                    resolve()
                 }
-                zip.close();
-            });
-        });
+                zip.close()
+            })
+        })
 
         zip.on('error', err => {
-            console.error('Error:', err);
-        });
-    });
-};
+            console.error('Error:', err)
+        })
+    })
+}
 
 // --- main ---
 
 const main = async () => {
-    fs.mkdirSync(path.join('temp', 'versions'), { recursive: true });
+    fs.mkdirSync(path.join('temp', 'versions'), { recursive: true })
 
     // also include latest pre-release or rc
-    const versionsRaw = (await getVersionList()).versions;
+    const versionsRaw = (await getVersionList()).versions
     const versions = versionsRaw.filter((v) => {
-        if (versionToNumber(v.id) < versionToNumber("1.7.10")) return false;
-        return v.type === "release";
+        if (versionToNumber(v.id) < versionToNumber("1.7.10")) return false
+        return v.type === "release"
     }).reverse() // from oldest to newest // from oldest to newest
     const latestVersionRaw = versionsRaw[0]!
     if (latestVersionRaw.type === 'snapshot' && (latestVersionRaw.id.includes('-pre') || latestVersionRaw.id.includes('-rc'))) {
         versions.push(latestVersionRaw)
     }
-    const latest = versions.at(-1)!;
+    const latest = versions.at(-1)!
     let i = 0
     for (const version of versions) {
-        await processVersionData(version, version === latest);
+        await processVersionData(version, version === latest)
     }
 
-    console.log('done');
-    fs.writeFileSync(path.join('./data/data-paths.json'), JSON.stringify(versionsDataPaths, null, 4));
+    console.log('done')
+    fs.writeFileSync(path.join('./data/data-paths.json'), JSON.stringify(versionsDataPaths, null, 4))
 }
 
-main();
+main()
