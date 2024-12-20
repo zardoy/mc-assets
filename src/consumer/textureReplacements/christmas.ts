ca833ca6f227b170b85add5487852c0b7aa0e1fa
Vitaly Turovsky <vital2580@icloud.com>
2024-12-20 22:33:39 +0300

    feat: add christmas texture replacements

---
+++ src/consumer/textureReplacements/christmas.ts
@@ -0,0 +1,10 @@
+//@ts-nocheck
+import chestNormal from '../other-textures/latest/entity/chest/christmas.png'
+import chestDoubleLeft from '../other-textures/latest/entity/chest/christmas_left.png'
+import chestDoubleRight from '../other-textures/latest/entity/chest/christmas_right.png'
+
+export default {
+    'entity/chest/normal': chestNormal,
+    'entity/chest/normal_left': chestDoubleLeft,
+    'entity/chest/normal_right': chestDoubleRight,
+}
